5af0a03099af492c2dcd5564fe9df323c1008e83
Matheus Boldrini <matheus.souza.boldrini@hotmail.com>
2022-02-13 17:53:56 -0300

    ao excluir agendamento, exclui evolução caso exista

--- src/modules/paciente_agendamento/services/DeleteAgendamentoService.ts
+++ src/modules/paciente_agendamento/services/DeleteAgendamentoService.ts
@@ -1,3 +1,4 @@
+import { EvolucaoRepository } from './../../paciente_evolucoes/typeorm/repositories/EvolucoesRepository';
 import { PacienteRepository } from '../../paciente/typeorm/repositories/PacienteRepository';
 import { AgendamentoRepository } from '../typeorm/repositories/AgendamentoRepository';
 import AppError from '@shared/errors/AppError';
@@ -12,8 +13,9 @@ interface IAgendamento {
 class DeleteAgendamentoService {
 	public async execute({ id, paciente_id, user_id }: IAgendamento): Promise<IAgendamento> {
 		const agendamentoRepository = getCustomRepository(AgendamentoRepository);
-
 		const pacienteRepo = getCustomRepository(PacienteRepository);
+		const evolucaoRepo = getCustomRepository(EvolucaoRepository);
+
 		const pacienteExiste = await pacienteRepo.findByIdAndUser({
 			id: paciente_id,
 			user_id,
@@ -31,6 +33,16 @@ class DeleteAgendamentoService {
 		}
 
 		agendamentoExiste.excluido = true;
+
+		const evolucaoExiste = await evolucaoRepo.findOne({
+			agendamento_id: agendamentoExiste.id,
+			user_id,
+			excluido: false,
+		});
+		if (evolucaoExiste) {
+			evolucaoExiste.excluido = true;
+			await evolucaoRepo.save(evolucaoExiste);
+		}
 
 		await agendamentoRepository.save(agendamentoExiste);
 
