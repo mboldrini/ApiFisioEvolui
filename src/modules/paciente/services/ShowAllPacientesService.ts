44d51bcb89f42eb58c08f4b65d0c62c80a8051a1
Matheus Boldrini <matheus.souza.boldrini@hotmail.com>
2022-04-20 11:14:05 -0300

    retorna o nome do tipo de atendimento ao inves do ID

--- src/modules/paciente/services/ShowAllPacientesService.ts
+++ src/modules/paciente/services/ShowAllPacientesService.ts
@@ -1,3 +1,4 @@
+import { TipoAtendimentoRepository } from './../../paciente_tipoAtendimento/typeorm/repositories/TipoAtendimentoRepository';
 import { PacienteRepository } from './../typeorm/repositories/PacienteRepository';
 import { Request, Response } from 'express';
 import AppError from '@shared/errors/AppError';
@@ -11,13 +12,14 @@ interface IRequest {
 interface IPctLista {
 	id: number;
 	nome: string;
-	tipoAtendimento: number;
+	tipoAtendimento: string | undefined;
 	logradouro: string;
 }
 
 class ShowAllPacienteService {
 	public async execute({ user_id }: IRequest): Promise<IPctLista[]> {
 		const pacienteRepo = getCustomRepository(PacienteRepository);
+		const tipoAtendimentoRepo = getCustomRepository(TipoAtendimentoRepository);
 
 		const pacienteEncontrado = await pacienteRepo.findAllPacientes(user_id);
 
@@ -25,10 +27,23 @@ class ShowAllPacienteService {
 			throw new AppError('Nenhum paciente cadastrado', 404);
 		}
 
+		const tipoAtendimentos = await tipoAtendimentoRepo.findAll({ user_id });
+		if (tipoAtendimentos?.length < 1) {
+			throw new AppError('Nenhum tipo de atendimento cadastrado', 404);
+		}
+
 		let pacienteLista = pacienteEncontrado.map(pct => ({
 			id: pct.id,
 			nome: pct.nome,
-			tipoAtendimento: 1,
+			tipoAtendimento: tipoAtendimentos
+				?.filter(tipo => {
+					if (tipo.id == pct.tipoAtendimento) {
+						return tipo.tipo;
+					}
+				})
+				.map(tipo => {
+					return tipo.tipo;
+				})[0],
 			logradouro: pct.logradouro,
 		}));
 
