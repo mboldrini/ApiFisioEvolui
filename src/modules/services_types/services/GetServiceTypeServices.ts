b3d2b276213c74eb442b9b2f348d61f3f70d51d9
Matheus Boldrini <matheus.souza.boldrini@hotmail.com>
2022-06-09 19:44:51 -0300

    exibe nome do metodo de pagamento

--- src/modules/services_types/services/GetServiceTypeServices.ts
+++ src/modules/services_types/services/GetServiceTypeServices.ts
@@ -1,9 +1,11 @@
+import { PaymentMethodRepository } from './../../payment_method/payment_method/typeorm/repositories/PaymentMethodRepository';
 import { ServicesTypesRepository } from '../typeorm/repositories/ServicesTypesRepository';
 import { UsersRepository } from '@modules/users/users/typeorm/repositories/UsersRepository';
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import ServicesTypes from '../typeorm/entities/ServicesTypes';
 import { TIMEZONE_LANGUAGE } from '@shared/DTO';
+import PaymentMethodUserRepository from './../../payment_method/paymentMethod_user/typeorm/repositories/PaymentMethodUserRepository';
 
 interface IRequest {
 	id: number;
@@ -14,6 +16,8 @@ class GetServicesTypeService {
 	public async execute({ id, user_code }: IRequest): Promise<Object> {
 		const usersRepo = getCustomRepository(UsersRepository);
 		const servicesTypesRepo = getCustomRepository(ServicesTypesRepository);
+		const paymentMethodRepo = getCustomRepository(PaymentMethodRepository);
+		const paymentMethodUserRepo = getCustomRepository(PaymentMethodUserRepository);
 
 		const userExist = await usersRepo.findOne({ user_code });
 		if (!userExist) throw new AppError("User don't exist", 404);
@@ -21,12 +25,23 @@ class GetServicesTypeService {
 		const serviceExist = await servicesTypesRepo.findOne({ id, user_id: userExist.user_id });
 		if (!serviceExist) throw new AppError("This service don't exist", 404);
 
+		const paymentMethodUserExist = await paymentMethodUserRepo.findOne({
+			id: serviceExist.id,
+			user_id: userExist.user_id,
+		});
+		if (!paymentMethodUserExist) throw new AppError('Tipo de pagamento não encontrado', 404);
+
+		const paymentMethodExist = await paymentMethodRepo.findOne({ id: paymentMethodUserExist.paymentMethod_id });
+		if (!paymentMethodUserExist) throw new AppError('Tipo de pagamento não encontrado - 2', 404);
+
 		let service = {
 			id: serviceExist.id,
 			name: serviceExist.name,
 			description: serviceExist.description,
 			duration: serviceExist.duration,
 			price: serviceExist.price,
+			paymentMethod_id: paymentMethodUserExist.id,
+			paymentMethod_name: paymentMethodExist?.name,
 			created_at: serviceExist.created_at.toLocaleString(TIMEZONE_LANGUAGE),
 			updated_at: serviceExist.updated_at.toLocaleString(TIMEZONE_LANGUAGE),
 		};
