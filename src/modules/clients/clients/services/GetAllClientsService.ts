9765d6b20fcceeeea9818ef541cd3d12ffe3e5bb
Matheus Boldrini <matheus.souza.boldrini@hotmail.com>
2022-08-07 10:23:24 -0300

    alteração na exibição de usuario

--- src/modules/clients/clients/services/GetAllClientsService.ts
+++ src/modules/clients/clients/services/GetAllClientsService.ts
@@ -1,33 +1,25 @@
+import { ServicesTypesRepository } from '@modules/services_types/typeorm/repositories/ServicesTypesRepository';
 import { ClientsRepository } from '../typeorm/repositories/ClientsRepository';
 import { UsersRepository } from '@modules/users/users/typeorm/repositories/UsersRepository';
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import Clients from '../typeorm/entities/Clients';
-import { TIMEZONE_LANGUAGE } from '@shared/DTO';
 
 interface IRequest {
 	user_code: string;
-}
-
-interface IClient {
-	id: number;
-	name: string;
-	document: string;
-	email: string;
-	celphone: string;
-	second_celphone: string;
-	instagram: string;
-	created_at: string;
-	updated_at: string;
 }
 
 class GetAllClientsService {
 	public async execute({ user_code }: IRequest): Promise<object> {
 		const usersRepo = getCustomRepository(UsersRepository);
 		const clientRepo = getCustomRepository(ClientsRepository);
+		const servicesRepo = getCustomRepository(ServicesTypesRepository);
 
 		const userExists = await usersRepo.findOne({ user_code });
 		if (!userExists) throw new AppError("User don't exist", 404);
+
+		const servicesList = await servicesRepo.find({ user_id: userExists.user_id });
+		console.log(servicesList);
 
 		const clientsList = await clientRepo.find({ user_id: userExists.user_id, enabled: true });
 
@@ -38,14 +30,18 @@ class GetAllClientsService {
 			document: client.document,
 			email: client.email,
 			celphone: client.celphone,
-			second_celphone: client.second_celphone,
-			instagram: client.instagram,
 			address: client.address,
-			latitude: client.latitude,
-			longitude: client.longitude,
-			created_at: client.created_at.toLocaleString(TIMEZONE_LANGUAGE),
-			updated_at: client.updated_at.toLocaleString(TIMEZONE_LANGUAGE),
+			serviceType_id: client.serviceType_id,
+			serviceType_name: servicesList
+				.filter(servicesList => {
+					if (servicesList.id == client.serviceType_id) return servicesList.name;
+				})
+				.map(servicesList => {
+					return servicesList.name;
+				})[0],
 		}));
+
+		// console.log(clientList);
 
 		return clientList;
 	}
