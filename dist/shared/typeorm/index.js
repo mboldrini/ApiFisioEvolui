b0c25b0cced237729e0dcc04bb3cc7e558664257
Matheus Boldrini <matheus.souza.boldrini@hotmail.com>
2022-10-11 19:35:27 -0300

    .

--- dist/shared/typeorm/index.js
+++ dist/shared/typeorm/index.js
@@ -4,7 +4,7 @@ var _typeorm = require("typeorm");
 const config = {
   type: 'mysql',
   port: process.env.DB_PORTA,
-  w: process.env.DB_HOST,
+  host: process.env.DB_HOST,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
